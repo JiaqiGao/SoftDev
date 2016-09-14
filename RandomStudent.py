b7d91d0e215b42592b5f3ab4dda8f73535e5c2f3
JiaqiGao <jiaqigao12@gmail.com>
2016-09-14 19:24:43 -0400

    Update

--- RandomStudent.py
+++ RandomStudent.py
@@ -1,29 +1,31 @@
 import random
 
-def RandomStudent(period):
-    if period == 4:
-        ary = [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson',
+CLASSES = {
+    4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson',
          'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin',
          'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth',
          'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj',
          'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward',
-         'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina']
-        return(ary[random.randint(0,len(ary))])
-    elif period == 8:
-        ary = ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki',
+         'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+    8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki',
         'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda',
         'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent',
         'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel',
         'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio',
-        'Kelly', 'William', 'Jordan', 'Haley', 'Henry']
-        return(ary[random.randint(0,len(ary))])
-    elif period == 9:
-        ary = ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C',
+        'Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+    9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C',
         'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha',
         'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry',
         'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine',
         'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L',
         'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
-        return(ary[random.randint(0,len(ary))])
-    return("Period input is invalid")
+}
+
+def RandomStudent(period):
+    if period == 4 or period == 8 or period == 9:
+        return(CLASSES[period][random.randint(0, len(CLASSES[period])-1)])
+    return("You can only input 4, 8, or 9")
     
+#print(RandomStudent(4))
+#print(RandomStudent(8))
+#print(RandomStudent(9))
