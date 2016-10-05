e477b9b6a175549bd3b991a0aa6bdfb23dbe4c36
Jiaqi Gao <jiaqigao12@gmail.com>
2016-10-04 20:24:42 -0400

    classwork

--- HW04-login/app.py
+++ HW04-login/app.py
@@ -2,6 +2,7 @@
 #SoftDev Pd08
 #HW4: Write a Flask app to facilitate simple logins via POST.
 
+import hashlib, csv 
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -9,12 +10,22 @@ app = Flask(__name__)
 def index():
     return render_template("form.html")
 
+csv = "static/storage.csv"
+
+#storage = open("static/storage.csv").read()
+#storage = storage.split(';')
+
+def hasher(passw):
+    hash_obj = hashlib.sha1(b'passw')
+    hex_dig = hash_obj.hexdigest()
+    return(hex_dig)
+
 @app.route("/auth", methods=['POST'])
 def authenticate():
-    if request.form["username"]=="doraemon" and request.form["password"]=="bluecatrobot":
-        return render_template("success.html", result="nice")
-    else:
-        return render_template("success.html", result="oh no")
+    fd = open(csv, 'a')    
+    fd.write(request.form["username"]+","+hasher(request.form["password"])+";")
+    fd.close()
+    return render_template("success.html", result="nice")
     
 if __name__ == "__main__":
     app.debug = True
